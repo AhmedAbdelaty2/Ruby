781b4ca1fa310b494195439e98ecd066ea9d122c
ahmedabdelaty2 <ahmedabdelaty2021@gmail.com>
2022-07-01 21:55:01 +0200

    lab2

---
+++ Lab2/CustomMath.rb
@@ -0,0 +1,31 @@
+class CustomMath
+
+  def calculate(first_number, second_number, operator)
+    begin
+      first_number = Integer(first_number)
+      second_number = Integer(second_number)
+    rescue ArgumentError => error
+      return error.message
+    end
+
+    case operator
+    when "+"
+      return first_number + second_number
+    when "-"
+      return first_number - second_number
+    when "*"
+      return first_number * second_number
+    when "/"
+      begin
+        return first_number / second_number
+      rescue ZeroDivisionError => error
+        return error.message
+      end
+    else
+      return "called not supported operator"
+    end
+  end
+end
+
+calculator = CustomMath.new
+p calculator.calculate("7", "2", "*")
